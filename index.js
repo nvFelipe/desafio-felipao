63510c5b1add7e2a1392a97bd07299aa66fe33d6
nvFelipe <feneves.fn@gmail.com>
2024-01-10 14:21:03 -0300

    inicio do desafio: adicionei as variaveis e peço as quantidades ao usuario

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-let nome = "Felipe"
-console.log("oi")
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Qual o nome do heroi? : ', (nomedoheroi) => {
+    rl.question('Qual o nivel do heroi? : ', (niveldoheroi) => {
+
+        const numeroInteiro = parseInt(niveldoheroi, 10);
+
+        console.log('O nome do heroi é:', nomedoheroi);
+        console.log('O nivel do heroi é:', niveldoheroi);
+
+  rl.close();
+    });
+});
